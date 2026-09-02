8a807817cd61c9211688d8a33edaa5e84013d907
Jakub Mikula <cubamic@wp.pl>
2026-09-02 22:06:48 +0200

    feat(notebooks): add initial 002_naive_eval notebook
    Also add useful functions, classess and static variables to src/nuteval/__init__.py

--- src/nuteval/__init__.py
+++ src/nuteval/__init__.py
@@ -1,11 +1,17 @@
 from nuteval.dataset import csv_to_jsonl, load_dataset
 from nuteval.cache import load_cached_runs, append_cached_run, get_cached_runs_per_meal
+from nuteval.evaluation import run_evaluation_for_model
+from nuteval.schemas import CachedRun
+from nuteval.config import MODELS
 
 
 __all__ = [
+    "MODELS",
+    "CachedRun",
     "csv_to_jsonl",
     "load_dataset",
     "load_cached_runs",
     "append_cached_run",
-    "get_cached_runs_per_meal"
+    "get_cached_runs_per_meal",
+    "run_evaluation_for_model"
 ]
